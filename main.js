fd7c1133c32e6dbe87cc78d97a4a694fca3f32f2
Alejandro Djukic <alejandrodjukic99@gmail.com>
2022-02-12 12:38:29 -0400

    Skills

--- main.js
+++ main.js
@@ -23,3 +23,31 @@ function Move(value) {
         track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
     }
 }
+
+// Skills 
+
+document.querySelector('#boton').addEventListener('click', Datos);
+
+function Datos(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'skills.json', true)
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let datos = JSON.parse(this.responseText);
+            let res = document.querySelector('#res');
+            res.innerHTML = '';
+
+            for (let item of datos) {
+                res.innerHTML += `
+                <tr>
+                    <td>${item.title}</td>
+                    <td>${item.level}</td>
+                </tr>
+                `
+            }
+
+        }
+    }
+
+}
